6a38ab948b2d666d54521abb2261cb0609ccc010
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-02-25 23:59:22 -0500

    Docs: add more explanation to our simple Constructor injection eg code

--- src/solid/Dependency_Inversion_principle/ConstructorInjectionSimplest.java
+++ src/solid/Dependency_Inversion_principle/ConstructorInjectionSimplest.java
@@ -5,7 +5,7 @@ package solid.dependency_inversion_principle;
  * receives the dependency via constructor (does not new it).
  */
 
-// Abstraction (the "wrap")
+// Abstraction (the "wrap")  // so like printer has one method print(String s) that must be implemented by the subclasses
 interface Printer {
     void print(String s);
 }
@@ -13,6 +13,7 @@ interface Printer {
 // it is the implementation of the interface
 // whatever you give to the printer, it will print it
 // so we can create like this: Printer printer = new ConsolePrinter(); printer.print("Hello");
+// console printer implements it and prints the string to the console rather than directly having print method 
 class ConsolePrinter implements Printer {
     @Override
     public void print(String s) {
@@ -24,7 +25,8 @@ class ConsolePrinter implements Printer {
 // so we can create like this: Greeter greeter = new Greeter(new ConsolePrinter()); greeter.say("Hello");
 // so we can say that the Greeter class is dependent on the Printer interface
 // and it is injected via constructor
-// so we can create like this: Greeter greeter = new Greeter(new ConsolePrinter()); greeter.say("Hello");   
+// so we can create like this: Greeter greeter = new Greeter(new ConsolePrinter()); greeter.say("Hello");  
+// this class will use the printer to print the message 
 class Greeter {
     private final Printer printer;
 
@@ -37,7 +39,10 @@ class Greeter {
         printer.print(msg);
     }
 }
-
+// this is the main class that will use the greeter to print the message using Printer class that implements the interface
+// so we can create like this: Greeter g = new Greeter(new ConsolePrinter()); g.say("Hello");
+// this class will use the printer to print the message 
+// so when g is created, the printer is passed in and it is used to print the message
 public class ConstructorInjectionSimplest {
     public static void main(String[] args) {
         Greeter g = new Greeter(new ConsolePrinter());
