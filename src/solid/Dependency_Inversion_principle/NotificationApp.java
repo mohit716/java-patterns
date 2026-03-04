8223431e476f76454c96505cb8b9f798cd462ff5
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-03-03 22:23:03 -0500

    docs: add explanation comments

--- src/solid/Dependency_Inversion_principle/NotificationApp.java
+++ src/solid/Dependency_Inversion_principle/NotificationApp.java
@@ -1,15 +1,15 @@
 // File: NotificationApp.java
-
+// this is the interface that will be implemented by the concrete classes
 interface MessageService {
     String getMessage();
 }
-
+// email service implements the message service interface means it will have the getMessage method
 class EmailService implements MessageService {
     public String getMessage() {
         return "Email message sent!";
     }
 }
-
+// this is the orchestrator class that will use the message service to send the message
 class NotificationManager {
     private final MessageService service;
 
@@ -22,7 +22,7 @@ class NotificationManager {
         System.out.println(service.getMessage());
     }
 }
-
+// this is the main class that will use the notification manager to send the message
 public class NotificationApp {
     public static void main(String[] args) {
         MessageService service = new EmailService();
