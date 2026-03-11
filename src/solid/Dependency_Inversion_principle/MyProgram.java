49b0a2796d57fe4085cd651d45661121538a5953
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-03-10 23:13:28 -0400

    docs: add notes in comments for the easy dip eg

--- src/solid/Dependency_Inversion_principle/MyProgram.java
+++ src/solid/Dependency_Inversion_principle/MyProgram.java
@@ -1,19 +1,24 @@
+// the method inside this must be implemented by someone implementing this interface
 interface MessageSender {
     void send(String msg);
 }
 
+// send method prints email: "whatever"
 class EmailSender implements MessageSender {
     public void send(String msg) {
         System.out.println("Email: " + msg);
     }
 }
-
+// SMS method prints SMS: "whatever"
 class SMSsender implements MessageSender {
     public void send(String msg) {
         System.out.println("SMS: " + msg);
     }
 }
-
+// below class has one job interface's instance creation (it use interface/parent for creation)
+// we did is that created interface object ,receiving type agnostic argument in constructor
+// and called aganostic.send method under another method of this class
+// so method to call another's method
 class NotificationService {
     private MessageSender sender;
 
@@ -33,3 +38,19 @@ public class MyProgram {
         service.notifyUser("Order placed");
     }
 }
+
+/*
+Summary:
+This design makes the three classes other than MyProgram
+more stable / less affected by change.
+
+Because in our version:
+
+EmailSender only knows how to send email
+
+SMSsender only knows how to send SMS
+
+NotificationService only knows “I use something that can send messages”
+
+MyProgram decides which actual sender to plug in
+*/
